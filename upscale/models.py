9ff37025341fbc35d404e131e6db20853beb5372
Arnav Kundalia <71081453+arnavkundalia@users.noreply.github.com>
2023-07-19 13:53:42 +0530

    Delete upscale directory

--- upscale/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Upscale(models.Model):
-    input_image = models.ImageField(upload_to='input_images/')
-    #output_image = models.ImageField(upload_to='output_images/')
